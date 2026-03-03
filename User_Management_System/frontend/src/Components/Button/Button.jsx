8ab51e8ef203e706869f9447e31b426df632b2ab
shafeeq <shafeeqap1986@gmail.com>
2026-03-03 12:08:19 +0530

    update

--- User_Management_System/frontend/src/Components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ className, onClick, children }) => {
-  return (
-    <button className={className} onClick={onClick}>
-      {children}
-    </button>
-  );
-};
-
-export default Button;
--- User_Management_System/frontend/src/utils/validation/forgotPasswordValidation.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-export const forgotPasswordValidation = Yup.object({
-  email: Yup.string()
-    .email("Please enter valid email")
-    .required("Please enter your email"),
-});
