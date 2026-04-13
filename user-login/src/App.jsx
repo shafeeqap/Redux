294bc86bbea1c13edff4b6fa3d9516178b1425db
shafeeq <shafeeqap1986@gmail.com>
2026-04-13 06:39:19 +0530

    update packages, fix axios vulnerability issues

--- user-login/src/App.jsx
+++ user-login/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
